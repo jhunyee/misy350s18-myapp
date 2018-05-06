eb926a5b412b9d2c20324ff5ac183fbb6524a2eb
Jhunyee Lin <jhunyee@gmail.com>
2018-05-05 23:47:51 -0400

    myappcode

---
+++ myapp/myapp.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+@app.route('/users/<string:username>')
+def users(username):
+    return render_template('users.html', uname=username)
+
+@app.route('/user')
+def user():
+    return "this is the page for users"
+
+if __name__ == '__main__':
+    app.run()
